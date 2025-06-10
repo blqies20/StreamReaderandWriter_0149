736b6ff38fedfc4ed473cc08ff904c1c9f584cca
blqies20 <balqis1maharani@gmail.com>
2025-06-10 11:25:17 +0700

    note pada manual exeption

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -7,15 +7,18 @@ int main() {
     try
     {
         cout << "Selamat Belajar di Prodi TI UMY" << endl;
-        throw 5;
+        throw 5; // melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
     catch(int a)
     {
+        // blok ini akan dieksekusi
         cout << "Pengecualian akan dieksekusi" << endl;
     }
     catch(...)
     {
+        // ini akan dieksekusi jika tidak ada pengecualian yang sesuai
+        /*jika selain integer maka block ini akan dieksekusi*/
         cout << "default Pengecualian dieksekusi" << endl;
     }
 
