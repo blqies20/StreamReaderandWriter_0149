812ad788059895fa468291f3ca3ab28b729cc6fc
blqies20 <balqis1maharani@gmail.com>
2025-06-10 11:20:48 +0700

    menambah note pada ioFile dengan path

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -10,18 +10,25 @@ int main(){
     cout << "Masukkan Nama File : ";
     cin >> NamaFile;
 
+    // membuka file dalam mode menulis
     ofstream outfile;
+    // menunjuk ke sebuah nama file
     outfile.open(NamaFile + ".txt", ios::out);
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
+    // inlimited loop untuk menulis
     while (true)
     {
         cout << "- ";
+        // mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
+        // loop akan berhenti jika anda memasukkan karakter q
         if (baris == "q") break;
+        // menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
+    //selesai dalam menulis sekarang tutup filenya
     outfile.close();
 
     ifstream infile;
