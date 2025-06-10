c0c307304be12ebeb9f819870de77ef7ce268139
blqies20 <balqis1maharani@gmail.com>
2025-06-10 10:32:40 +0700

    implementasi pembacaan file disertai validasi pembukaan file menggunakan if-else

--- ioFile.cpp
+++ ioFile.cpp
@@ -23,4 +23,15 @@ int main() {
     infile.open("data.txt");
 
     cout << endl << "> = Membuka dan membaca file " << endl;
+
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << '\n';
+        }
+        infile.close();
+    }
+    else cout << "Unable to open file";
+    return 0;
 };
