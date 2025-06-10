5f65ff29eaf881d60ac80c4c540070b4d3d72bd9
blqies20 <balqis1maharani@gmail.com>
2025-06-10 11:27:27 +0700

    note di library exception dan array

--- exeptionStandart.cpp
+++ exeptionStandart.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <exception>
+// untuk objek exception yang akan digunakan
 #include <array>
+// untuk objek array yang akan kita gunakan
 using namespace std;
 
 int main()
